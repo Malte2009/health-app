58983f5b7e81c53fc2f6c409547027c3cb19984c
Malte2009 <malte.roesgen@gmail.com>
2025-06-27 16:58:54 +0200

    fix: update host variable to use hostname instead of host for API base URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,14 @@
 import { getCookie } from "@/utility/cookie";
 import axios from "axios";
 
-const host = window.location.host;
+const host = window.location.hostname;
 
 const api = axios.create({
   baseURL: `http://${host}:3000/api`,
   withCredentials: true,
   headers: {
-    "authorization": getCookie("token") || ""
-  }
+    authorization: getCookie("token") || "",
+  },
 });
 
 export default api;
