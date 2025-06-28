6cbcb6f9a238b51e512bd3ac60ad6f1589895c9c
Malte2009 <malte.roesgen@gmail.com>
2025-06-28 17:10:28 +0200

    feat: add functionality to change exercise order and update training data accordingly

--- src/types/exerciseType.ts
+++ src/types/exerciseType.ts
@@ -47,6 +47,32 @@ export type changeExerciseOrderResponse = {
   order: number;
 };
 
+export type changeExerciseOrderRequestType = {
+  order: number;
+};
+
+export type changeExerciseOrderResponse = {
+  sets: {
+    id: string;
+    userId: string;
+    createdAt: Date;
+    changedAt: Date | null;
+    order: number;
+    type: string | null;
+    weight: number;
+    exerciseId: string;
+    reps: number;
+  }[];
+} & {
+  id: string;
+  userId: string;
+  trainingId: string;
+  name: string;
+  createdAt: Date;
+  changedAt: Date | null;
+  order: number;
+};
+
 export type changeExerciseResponse = {
   id: string;
   name: string;
