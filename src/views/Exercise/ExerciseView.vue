d71a73f8fe31bbe8829c25b498d058c6ff96772b
Malte Roesgen <malte.roesgen@gmail.com>
2026-01-08 11:46:48 +0100

    feat: implement ExerciseView with chart integration and responsive layout

--- src/views/Exercise/ExerciseView.vue
+++ src/views/Exercise/ExerciseView.vue
@@ -1,6 +1,50 @@
-<template></template>
+<template>
+  <div class="exerciseView">
+    <h1>Exercise View</h1>
+
+    <div class="exercise">
+      <h2>Main Panel</h2>
+    </div>
+    <div class="chart">
+      <h2>Chart Panel</h2>
+
+      <Chart :chart-data="chartData" :width="width" :height="height"/>
+    </div>
+  </div>
+</template>
 
 <script setup lang="ts">
+import Chart from "@/components/General/Chart.vue";
+import { useExerciseStore } from "@/stores/exerciseStore.ts";
+import { onBeforeMount, ref } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const exerciseStore = useExerciseStore();
+
+const exerciseName = route.params.name as string;
+
+const chartData = ref({
+  xData: [] as number[],
+  yData: [] as number[],
+});
+
+const width = ref(0);
+const height = ref(0);
+
+function getChartData() {
+  const exercise = exerciseStore.getExerciseByName(exerciseName);
+
+  if (exercise) {
+
+  }
+}
+
+onBeforeMount(() => {
+  width.value = window.innerWidth * 0.8;
+  height.value = window.innerHeight * 0.6;
+})
 </script>
 
 <style scoped></style>
